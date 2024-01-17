2aba9b7656eb15870d30a85afff9b33e25ee949b
nal-abs <nal@abstractit.in>
2024-01-17 13:10:58 +0530

    Added some config files.

--- createSprite.js
+++ createSprite.js
@@ -1,35 +1,44 @@
 const Spritesmith = require('spritesmith');
 const fs = require('fs').promises;
-const sharp = require('sharp'); 
+const sharp = require('sharp');
 
-const createSprite = async() => {   
-  try {     
-    const folderPath = 'Images';     
-    const files = await fs.readdir(folderPath); 
-    const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));     
-    const spriteImages = imageFiles.map((image) => `Images/${image}`);
-    const result = await new Promise((resolve, reject) => {     
-        Spritesmith.run({ src: spriteImages }, (err, result) => {     
-            err ?  reject(err) : resolve(result)
-           });     
-          });     
-                
-    await fs.writeFile('sprite.png', result.image);  
-    await fs.writeFile('sprite.json', JSON.stringify(result.coordinates));     
-    const pngBuffer = await fs.readFile('sprite.png');    
-    const webpBuffer = await sharp(pngBuffer)       
-    .toFormat('webp', { quality: 100 })       
-    .toBuffer();    
-    await fs.writeFile('sprite.webp', webpBuffer);        
-    } 
-  catch (error) {  
-        console.error('Error:', error); 
-        } 
-      }
+const getImages = async () => {
+	try {
+		const folderPath = 'Images';
+		const files = await fs.readdir(folderPath);
+		const filteredImage = files.filter((file) => (/\.(jpg|jpeg|png|gif)$/i).test(file));
+		const images = filteredImage.map((image) => `Images/${ image }`);
 
-createSprite()
+		return images;
+	}
+	catch (error) {
+		throw new Error('Error reading folder:', error);
+	}
+};
 
+const createSprite = async () => {
+	try {
+		const images = await getImages();
 
+		const detail = await new Promise((resolve, reject) => {
+			Spritesmith.run({ src: images }, (err, result) => {
+				err ? reject(err) : resolve(result);
+			});
+		});
 
+		await fs.writeFile('sprite.png', detail.image);
+		await fs.writeFile('sprite.json', JSON.stringify(detail.coordinates));
 
+		const pngBuffer = await fs.readFile('sprite.png');
+		const webpBuffer = await sharp(pngBuffer)
+			.toFormat('webp', { quality: 100 })
+			.toBuffer();
 
+		await fs.writeFile('sprite.webp', webpBuffer);
+	}
+	catch (error) {
+		throw new Error('Error:', error);
+	}
+};
+
+createSprite();
